29e97b558558b2b712299d148b2f20e768830070
manvinder086 <manvindersingh086@gmail.com>
2025-02-08 12:11:50 +1100

    creating dependent jobs

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Query, Cookie, Response
+from fastapi import FastAPI, Query, Depends, Cookie, Response,status, Form
 from typing import Annotated
 from pydantic import BaseModel, Field
 from typing import Annotated
@@ -24,7 +24,7 @@ EmployeeList = [
 ]
 
 
-@app.get('/getEmp')
+@app.get('/getEmp', status_code=status.HTTP_302_FOUND)
 async def getEmployeeById(empId: Annotated[int, Query(gt=5)]):
     employee = []
     for emp in EmployeeList:
@@ -59,3 +59,54 @@ async def set_cookie(response: Response):
     # Set a cookie named "my_cookie" with value "fastapi_value"
     response.set_cookie(key="sessionId", value="1234567")
     return {"message": "Cookie has been set"}
+
+
+## USING FORMS
+
+
+@app.post("/login")
+async def login(username: Annotated[str, Form()], password: Annotated[str, Form()]):
+    return { "username" : username , "password": password}
+
+
+class FormModels(BaseModel):
+    username: str
+    password: str
+
+@app.post("/loginWithFormModel")
+async def loginModel(data: Annotated[FormModels, Form()]):
+    return { "username" : data.username , "password": data.password}
+
+
+###  FASTAPI DEPENDENCY INJECTION
+
+# async def common_parameters(q:str | None = None, skip : int =0, limit: int = 100):
+#     return {"q" : q, "skip": skip, "limit": limit}
+
+
+# @app.get("/items/")
+# async def read_items(commons: Annotated[dict, Depends(common_parameters)]):
+#     return commons
+
+
+# @app.get("/users/")
+# async def read_users(commons: Annotated[dict, Depends(common_parameters)]):
+#     return commons
+
+
+### CLASS AS A DEPENDENCY INJECTION
+
+class common_parameters:
+    def __init__(self, q: str | None = None, skip : int = 0, limit: int = 100):
+        self.q = q
+        self.skip = skip
+        self.limit = limit
+
+@app.get("/items/")
+async def read_items(commons: Annotated[dict, Depends(common_parameters)]):
+    return commons
+
+
+@app.get("/users/")
+async def read_users(commons: Annotated[dict, Depends(common_parameters)]):
+    return commons
